ce18ac781b2c04fa6d58eda9c2b07e5d56e849ab
michael.zhao <michael.zhao@ximalaya.com>
2022-09-28 23:17:52 +0800

    fix

--- step1_gen_train_test.py
+++ step1_gen_train_test.py
@@ -8,7 +8,7 @@ from smbert.common.tokenizers import Tokenizer
 def random_wrong(text):
     tokenizer = Tokenizer(VocabPath)
     length = len(text)
-    position = random.randint(0, length-1)
+    position = random.randint(0, length - 1)
     number = random.randint(672, 7992)
     text = list(text)
     text[position] = tokenizer.id_to_token(number)
@@ -17,7 +17,7 @@ def random_wrong(text):
 
 
 def gen_train_test():
-    # f_train = open(CorpusPath, 'w', encoding='utf-8')
+    f_train = open(CorpusPath, 'w', encoding='utf-8')
     f_test = open(TestPath, 'w', encoding='utf-8')
     with open(SourcePath, 'r', encoding='utf-8') as f:
         for line in f:
@@ -26,12 +26,12 @@ def gen_train_test():
             if rad < 1:
                 # f_test.write(line + '-***-' + random_wrong(line) + '\n')
                 f_test.write(line + '\n')
-            # else:
-            #     i += 1
-            #     f_train.write(line + '\n')
+            else:
+                f_train.write(line + '\n')
 
-    # f_train.close()
+    f_train.close()
     f_test.close()
+
 
 if __name__ == '__main__':
     print(len(open(VocabPath, 'r', encoding='utf-8').readlines()))
